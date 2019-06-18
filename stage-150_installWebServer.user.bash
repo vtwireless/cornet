8d7a91046b62fe91bd5cce933cb3bd6f8417fc16
Lance Arsenault <lanceman@vt.edu>
2019-06-18 16:46:39 +0000

    fixed webServer HTTPS_HOSTNAME

--- stage-150_installWebServer.user.bash
+++ stage-150_installWebServer.user.bash
@@ -8,7 +8,7 @@ mkdir -p $CORNET_PREFIX/bin
 sed\
  -e "s!@DEFAULT_SERVER_ADDRESS@!${DEFAULT_SERVER_ADDRESS}!g"\
  -e "s!@DEFAULT_HTTPS_PORT@!${DEFAULT_HTTPS_PORT}!g"\
- -e "s!@DEFAULT_HTTPS_ADDRESS@!${DEFAULT_HTTPS_ADDRESS}!g"\
+ -e "s!@DEFAULT_HTTPS_HOSTNAME@!${DEFAULT_HTTPS_HOSTNAME}!g"\
  -e "s!@CRTS_PREFIX@!${CRTS_PREFIX}!g"\
  cornet_webServer.in\
  > cornet_webServer
