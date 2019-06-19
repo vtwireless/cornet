df03d2d17db2b8a1495efbae9351e92109999ed0
Lance Arsenault <lanceman@vt.edu>
2019-06-19 11:54:27 -0400

    Saving Work

--- stage-150_installWebServer.user.bash
+++ stage-150_installWebServer.user.bash
@@ -6,10 +6,7 @@ mkdir -p $CORNET_PREFIX/bin
 
 
 sed\
- -e "s!@DEFAULT_SERVER_ADDRESS@!${DEFAULT_SERVER_ADDRESS}!g"\
  -e "s!@DEFAULT_HTTPS_PORT@!${DEFAULT_HTTPS_PORT}!g"\
- -e "s!@DEFAULT_HTTPS_HOSTNAME@!${DEFAULT_HTTPS_HOSTNAME}!g"\
- -e "s!@CRTS_PREFIX@!${CRTS_PREFIX}!g"\
  bin/cornet_webServer.in\
  > bin/cornet_webServer
 
