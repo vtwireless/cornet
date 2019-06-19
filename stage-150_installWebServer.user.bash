2c31891f7a21eb20b88d7d0ede7426c82ccc68a8
Lance Arsenault <lanceman@vt.edu>
2019-06-19 12:12:22 -0400

    Saving Work

--- stage-150_installWebServer.user.bash
+++ stage-150_installWebServer.user.bash
@@ -5,10 +5,19 @@ set -ex
 mkdir -p $CORNET_PREFIX/bin
 
 
+cat << EOF > bin/cornet_webServer
+#!/bin/bash
+
+# This is a generated file.
+
+EOF
+
+
+
 sed\
  -e "s!@DEFAULT_HTTPS_PORT@!${DEFAULT_HTTPS_PORT}!g"\
  bin/cornet_webServer.in\
- > bin/cornet_webServer
+ >> bin/cornet_webServer
 
 chmod 755 bin/cornet_webServer
 cp bin/cornet_webServer $CORNET_PREFIX/bin
